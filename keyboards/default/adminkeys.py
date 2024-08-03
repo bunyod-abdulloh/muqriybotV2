961d755a4e757660057272d27444df20a01e1470
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-08-03 20:24:09 +0500

    Updated

--- keyboards/default/adminkeys.py
+++ keyboards/default/adminkeys.py
@@ -4,9 +4,7 @@ adm_adm = ReplyKeyboardMarkup(
     resize_keyboard=True,
     one_time_keyboard=True
 )
-adm_adm.row("Forward ON", "Forward OFF")
-adm_adm.row("MediaGroup ON", "MediaGroup OFF")
-adm_adm.row("ID ON", "ID OFF")
+
 adm_adm.row("Sending messages", "Cancel sending messages")
 adm_adm.row("Count_all_users", "Count_blocked_users")
 adm_adm.add("Sql buttons", "🏡 Бош саҳифа")

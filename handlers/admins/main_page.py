63fd0a3437910afb06fc9410fc9abbd15f2376ec
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-12-28 12:57:34 +0500

    Updated

--- handlers/admins/main_page.py
+++ handlers/admins/main_page.py
@@ -47,21 +47,24 @@ async def elonj(message: types.Message, state: FSMContext):
     await db.update_status_true()
     all_users = await db.select_all_users()
     success_count, failed_count = 0, 0
+
     for index, user in enumerate(all_users, start=1):
         try:
-            success_count += 1
             await message.copy_to(chat_id=user['telegram_id'])
+            success_count += 1
         except aiogram.exceptions.BotBlocked:
             failed_count += 1
-            await db.delete_user_tgid(user['telegram_id'])
-        else:
+            await db.delete_user_tgid(user['telegram_id'])  # Faqat bloklangan foydalanuvchilar o'chiriladi
+        except Exception as e:
             failed_count += 1
-            await db.delete_user_tgid(user['telegram_id'])
+
         if index % 1500 == 0:
-            await asyncio.sleep(30)
-        await asyncio.sleep(0.05)
+            await asyncio.sleep(30)  # Yuborishni vaqtincha to'xtatish
+        await asyncio.sleep(0.05)  # Bir xabar yuborish orasida kichik kechikish
+
     await message.answer(
-        f"Xabar {success_count} ta foydalanuvchiga yuborildi!"
+        f"Xabar {success_count} ta foydalanuvchiga muvaffaqiyatli yuborildi!\n"
+        f"{failed_count} ta foydalanuvchiga yuborilmadi."
     )
     await db.update_status_false()
 
@@ -73,17 +76,16 @@ async def count_users_handler(message: types.Message):
         text=f"Umumiy foydalanuvchilar soni: {count} ta"
     )
 
+@dp.message_handler(F.text == "add_users", user_id=SUPER_ADMIN)
+async def add_users(message: types.Message):
+    count = 0
+    for user in users_list:
+        try:
+            count += 1
+            await db.add_user(telegram_id=user)
+        except asyncpg.exceptions.UniqueViolationError:
+            pass
+        else:
+            pass
 
-# @dp.message_handler(F.text == "add_users", user_id=SUPER_ADMIN)
-# async def add_users(message: types.Message):
-#     count = 0
-#     for user in users_list:
-#         try:
-#             count += 1
-#             await db.add_user(telegram_id=user)
-#         except asyncpg.exceptions.UniqueViolationError:
-#             pass
-#         else:
-#             pass
-#
-#     await message.answer(text=f"{count} ta foydalanuvchi qo'shildi!")
+    await message.answer(text=f"{count} ta foydalanuvchi qo'shildi!")
